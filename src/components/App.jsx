b0eefb129018c99143b933ae4db1b3eb789bdd11
AnastasiiaPavlovska <nastiapavlovska446@gmail.com>
2023-08-06 19:40:18 +0200

    Intermediate

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import { Profile } from "./Profile/Profile";
+import { Statistics } from "./Statistics/Statistics";
+import { FriendList } from "./FriendList/FriendList";
+import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
+import { GlobalStyle } from "./GlobalStyle";
+import user from "./Data/user.json";
+import data from "./Data/data.json";
+import friends from "./Data/friends.json";
+import transactions from "./Data/transactions.json";
+
+export const App = () => {
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+      <GlobalStyle />
+    </div>
+  );
+};
