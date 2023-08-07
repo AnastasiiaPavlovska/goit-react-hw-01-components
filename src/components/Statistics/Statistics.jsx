eeb23c9688a1a99db8929e16945daef46213d20b
AnastasiiaPavlovska <nastiapavlovska446@gmail.com>
2023-08-07 12:30:17 +0200

    front: Add Statistics

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import { Section, StatList, ListItem } from "./Statistics.styled"
+
+
+export const Statistics = ({ stats, title }) => {
+    return (
+        <Section>
+            <h2 >{title}</h2>
+
+            <StatList>
+                {stats.map(({ id, label, percentage }) => (
+                    <ListItem key={id} color={label}>
+                        <span >{label} </span>
+                        <span >{percentage}%</span>
+                    </ListItem>))}
+            </StatList>
+        </Section>
+    );
+}
+
+Statistics.propTypes = {
+    stats: PropTypes.arrayOf(
+        PropTypes.exact({
+            label: PropTypes.string.isRequired,
+            id: PropTypes.string.isRequired,
+            percentage: PropTypes.number.isRequired
+        }),
+    )
+}
